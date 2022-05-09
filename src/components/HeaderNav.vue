2d8d1e2f4a73f851d872bf22ab27159a038edfcc
Alabo Excel <iamalaboexcel@gmail.com>
2022-05-09 12:15:03 +0100

    nav commit

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -17,10 +17,12 @@
 
         <div class="lg:ml-8 lg:mr-0 mr-6 flex lg:w-80 justify-evenly">
           <div class="p-2">
-            <a href="https://getartizan-artisan.netlify.app/"> Post a Job </a>
+            <a href="https://getartizan-artisan.netlify.app/auth/login/">
+              Post a Job
+            </a>
           </div>
           <div>
-            <a href="https://getartizan-cutomers.netlify.app/">
+            <a href="https://getartizan-cutomers.netlify.app/auth/login/">
               <input
                 type="button"
                 class="bg-blue p-2 lg:px-8 px-4 text-white"
