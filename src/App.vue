47e347103cde7c4365f7d0b554bfd4e2a8fd1af3
Alabo Excel <iamalaboexcel@gmail.com>
2022-06-04 23:38:23 +0100

    header nav and CTA

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script setup>
 import HeaderNav from "./components/HeaderNav.vue";
+import MobileNav from "./components/MobileNav.vue";
+
 import Hero from "./components/Hero.vue";
 import Footer from "./components/Footer.vue";
 
 components: {
-  HeaderNav, Hero, Footer;
+  HeaderNav, Hero, Footer, MobileNav;
 }
 </script>
 
 <template>
   <div>
-    <HeaderNav />
+    <HeaderNav class="desktop" />
+    <MobileNav class="mobile" />
     <Hero />
     <div>
       <div class="text-center lg:mx-0 mx-6 my-5">
@@ -23,45 +26,39 @@ components: {
         </div>
       </div>
       <div class="lg:flex lg:my-0 my-10 justify-evenly lg:p-16 p-4">
-        <div class="lg:w-1/5 text-center lg:my-0 my-8 ">
-          <img
-            class="w-16 mx-auto"
-            src="./assets/features/tool-box.png"
-            alt=""
-          />
+        <div class="lg:w-1/5 p-4 lg:my-0 my-8 shadow-xl rounded-md">
+          <img class="w-10" src="./assets/features/tool-box.png" alt="" />
           <div class="text-lg my-2 font-bold">Carpenter</div>
-          <!-- <div class="text-m">
-            Find new customers and build your email list with lead generation ..
-          </div> -->
+          <div class="text-m">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+            enim quas animi in illum alias expedita
+          </div>
         </div>
-        <div class="lg:w-1/5 text-center lg:my-0 my-8">
-          <img
-            class="w-16 mx-auto"
-            src="./assets/features/cleaning.png"
-            alt=""
-          />
+        <div class="lg:w-1/5 p-4 lg:my-0 my-8 shadow-xl rounded-md">
+          <img class="w-10" src="./assets/features/cleaning.png" alt="" />
           <div class="text-lg my-2 font-bold">Cleaner</div>
-          <!-- <div class="text-m">
-            Find new customers and build your email list with lead generation ..
-          </div> -->
+          <div class="text-m">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+            enim quas animi in illum alias expedita
+          </div>
         </div>
-        <div class="lg:w-1/5 text-center lg:my-0 my-8">
-          <img class="w-16 mx-auto" src="./assets/features/helmet.png" alt="" />
+
+        <div class="lg:w-1/5 p-4 lg:my-0 my-8 shadow-xl rounded-md">
+          <img class="w-10" src="./assets/features/helmet.png" alt="" />
           <div class="text-lg my-2 font-bold">Electrician</div>
-          <!-- <div class="text-m">
-            Find new customers and build your email list with lead generation ..
-          </div> -->
+          <div class="text-m">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+            enim quas animi in illum alias expedita
+          </div>
         </div>
-        <div class="lg:w-1/5 text-center lg:my-0 my-8">
-          <img
-            class="w-16 mx-auto"
-            src="./assets/features/responsive.png"
-            alt=""
-          />
+
+        <div class="lg:w-1/5 p-4 lg:my-0 my-8 shadow-xl rounded-md">
+          <img class="w-10" src="./assets/features/responsive.png" alt="" />
           <div class="text-lg my-2 font-bold">Web Developer</div>
-          <!-- <div class="text-m">
-            Find new customers and build your email list with lead generation ..
-          </div> -->
+          <div class="text-m">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+            enim quas animi in illum alias expedita
+          </div>
         </div>
       </div>
     </div>
@@ -69,32 +66,56 @@ components: {
     <div class="lg:flex lg:m-12 m-6 lg:mx-20 justify-between">
       <div class="lg:w-full my-auto">
         <div class="lg:text-6xl text-3xl my-3">What is GetArtisan?</div>
-        <div class="lg:text-2xl text-lg my-8 text-gray-500">
+        <div class="lg:text-2xl text-thin text text-lg my-8 text-gray-500">
           About GetArtisan is an online platform that connects customers across
           the globe. We help find artisans that best deliver the services you
-          need in your location of choice.
+          need in your location of choice. Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Magni est molestiae quos repellat
+          ducimus culpa dicta, voluptatum, incidunt optio, velit dignissimos
+          sint fugiat laudantium!
         </div>
-        <div class="lg:text-2xl text-lg text-blue mb-5">Become an Artisian</div>
+        <button
+          class="
+            lg:text-2xl
+            bg-blue
+            lg:p-4
+            p-2
+            rounded-md
+            text-lg text-white
+            mb-5
+          "
+        >
+          Become an Artisian
+        </button>
       </div>
-      <div class="lg:w-full">
-        <img
-          class="ml-auto"
-          src="./assets/collage.jpg"
-          alt=""
-        />
+      <div class="lg:w-full mt-6">
+        <img class="ml-auto rounded-md" src="./assets/collage.jpg" alt="" />
       </div>
     </div>
 
-    <div class="my-32">
+    <div class="my-32 bg-gray-100 py-10 lg:pb-20">
       <div class="text-center lg:m-0 m-6 my-5">
-        <div class="lg:text-5xl text-3xl font-bold lg:w-1/2 mx-auto capitalize my-4">
+        <div
+          class="
+            lg:text-5xl
+            text-3xl
+            font-bold
+            lg:w-1/2
+            mx-auto
+            capitalize
+            my-4
+            text-blue
+          "
+        >
           Get artisan is free simple and easy to use
         </div>
       </div>
       <div class="lg:flex lg:my-0 lg:my-10 justify-evenly lg:p-16 p-4">
         <div class="lg:w-1/5 text-center lg:my-0 my-8">
           <img class="w-16 mx-auto" src="./assets/desc/booking.png" alt="" />
-          <div class="text-2xl my-2 font-bold">Search Artisans by location</div>
+          <div class="text-2xl my-2 font-bold text-blue">
+            Search Artisans by location
+          </div>
           <div class="text-m">
             here are thousands of artisans in various locations waiting for a
             booking from you
@@ -102,14 +123,16 @@ components: {
         </div>
         <div class="lg:w-1/5 text-center lg:my-0 my-8">
           <img class="w-16 mx-auto" src="./assets/desc/search.png" alt="" />
-          <div class="text-2xl my-2 font-bold">Book any artisan of choice</div>
+          <div class="text-2xl my-2 font-bold text-blue">
+            Book any artisan of choice
+          </div>
           <div class="text-m">
             Select from the list of artisans in your specified location.
           </div>
         </div>
         <div class="lg:w-1/5 text-center lg:my-0 my-8">
           <img class="w-16 mx-auto" src="./assets/desc/phone-call.png" alt="" />
-          <div class="text-2xl my-2 font-bold">
+          <div class="text-2xl my-2 font-bold text-blue">
             Recieve a call from the verified artisan
           </div>
           <div class="text-m">
@@ -118,7 +141,7 @@ components: {
         </div>
       </div>
       <div class="text-center">
-        <button class="bg-blue p-5 text-white text-lg rounded-md">
+        <button class="bg-blue lg:p-5 p-2 text-white text-lg rounded-md">
           Get started now for free
         </button>
       </div>
@@ -126,19 +149,34 @@ components: {
 
     <div class="lg:flex lg:m-12 m-6 lg:mx-20 justify-between my-20">
       <div class="lg:w-full my-auto lg:mr-4">
-        <div class="lg:text-5xl text-3xl my-3">Daily life made simpler</div>
-        <div class="lg:text-xl text-lg my-8 text-gray-500">
+        <div
+          class="lg:text-5xl lg:text-left text-center text-3xl mt-3 text-blue"
+        >
+          Daily life made simpler
+        </div>
+        <div
+          class="
+            lg:text-xl lg:text-left
+            text-center text-lg
+            my-8
+            text-gray-500 text
+          "
+        >
           Looking to get the job done by a genuine professional in his field?
           Search no more, We're here to simplify your search by offering the
           very best.
         </div>
         <div>
-          <ul class="lg:text-2xl text-lg mx-5 lg:mb-0 mb-8">
-            <li class="my-2">Choose from our database of verified artisans</li>
-            <li class="my-2">
+          <ul class="lg:text-2xl text-lg lg:mb-0 mb-8">
+            <li class="my-2 text-gray-500 text text-xl">
+              Choose from our database of verified artisans
+            </li>
+            <li class="my-2 text-gray-500 text text-xl">
               Schedule and fix a time that works best for you
             </li>
-            <li class="my-2">Discuss with the artisan and get your job done</li>
+            <li class="my-2 text-gray-500 text text-xl">
+              Discuss with the artisan and get your job done
+            </li>
           </ul>
         </div>
       </div>
@@ -146,8 +184,49 @@ components: {
         <img class="ml-auto" src="./assets/easier.png" alt="" />
       </div>
     </div>
-    <div>
+    <div class="my-20">
+      <div class="lg:text-5xl text-3xl text-center tect-blue">
+        Ready to get Started?
+      </div>
+      <div class="lg:flex justify-evenly my-20 lg:m-0 m-4">
+        <div class="w-full">
+          <img
+            class="lg:w-96 w-44 mx-auto"
+            src="./assets/features/tool-box.png"
+            alt=""
+          />
+          <div class="lg:w-96 mx-auto text-center text lg:text-lg text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            architecto obcaecati laboriosam vero nobis quaerat!
+          </div>
+          <div class="text-center my-4">
+            <button
+              class="bg-blue mx-auto w-44 lg:p-4 p-2 text-white rounded-md"
+            >
+              Sign Up as User
+            </button>
+          </div>
+        </div>
 
+        <div class="w-full">
+          <img
+            class="lg:w-96 w-44 mx-auto"
+            src="./assets/features/tool-box.png"
+            alt=""
+          />
+          <div class="lg:w-96 mx-auto text-center text lg:text-lg text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            architecto obcaecati laboriosam vero nobis quaerat!
+          </div>
+          <div class="text-center my-4">
+            <button
+              class="bg-blue mx-auto w-44 lg:p-4 p-2 text-white rounded-md"
+            >
+              Sign Up as Artisian
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
     <Footer />
   </div>
@@ -160,7 +239,25 @@ components: {
   box-shadow: 0px 20px 50px rgba(198, 217, 225, 0.3);
   border-radius: 5px;
 }
-ul li {
-  list-style-type: square;
+ul li:before {
+  content: "✓";
+  font-size: 30px;
+}
+.text {
+  font-family: "Poppins", sans-serif;
+}
+.desktop {
+  display: block;
+}
+.mobile {
+  display: none;
+}
+@media (max-width: 600px) {
+  .desktop {
+    display: none;
+  }
+  .mobile {
+    display: block;
+  }
 }
 </style>
---
+++ src/components/MobileNav.vue
@@ -0,0 +1,175 @@
+<template>
+  <div class="content h-[60px] w-full bg-white">
+    <div class="container pt-4 flex justify-between">
+      <router-link to="/">
+        <img class="w-40 lg:w-32" src="../assets/logo.png" alt="" />
+      </router-link>
+      <div @click="openNav" class="flex burger lg:mt-0 mr-8">
+        <img class="w-6 lg:w-32" src="../assets/bar-black.svg" alt="" />
+      </div>
+    </div>
+    <div id="mySidenav" :class="[!state.open ? 'sidenav' : 'sidenavOpen']">
+      <div class="lg:mx-20 mx-8 flex justify-between">
+        <router-link to="/">
+          <!-- <img class="w-24 lg:w-32" src="../static/logo-black.png" alt="" /> -->
+          <img class="w-[93px]" src="/img/logo.png" alt="" />
+        </router-link>
+        <a
+          href="javascript:void(0)"
+          class="closebtn lg:-mt-10 -mt-6"
+          @click="closeNav"
+          >&times;</a
+        >
+      </div>
+      <div class="lg:mx-20 mx-8 mt-10">
+        <div
+          class="normal-case lg:text-8xl text-2xl lg:flex font-inter text-black"
+        >
+          <div class="p-2">Home</div>
+          <div class="p-2">About Us</div>
+          <div class="p-2">Categories</div>
+          <div class="p-2">Task finder</div>
+          <div class="my-5">
+            <input
+              type="button"
+              class="bg-blue p-2 lg:px-8 px-4 text-white"
+              value="Become an Artisan"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup  >
+import { reactive } from "vue";
+// import links from '../static';
+// links.pop()
+const state = reactive({
+  open: false,
+});
+const openNav = () => {
+  state.open = true;
+};
+const closeNav = () => {
+  state.open = false;
+  console.log("Fired");
+};
+</script>
+
+<style scoped>
+.sidenav {
+  height: 100%; /* 100% Full-height */
+  width: 0; /* 0 width - change this with JavaScript */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Stay on top */
+  top: 0; /* Stay at the top */
+  left: 0;
+  background-color: #fff; /* Black*/
+  overflow-x: hidden; /* Disable horizontal scroll */
+  padding-top: 60px; /* Place content 60px from the top */
+  transition: 0.5s; /* 0.5 second transition effect to slide in the sidenav */
+}
+/* The navigation menu links */
+/* .sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+} */
+/* When you mouse over the navigation links, change their color */
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+/* Position and style the close button (top right corner) */
+.sidenav .closebtn {
+  font-size: 50px;
+  color: #000;
+}
+/* Style page content - use this if you want to push the page content to the right when you open the side navigation */
+#main {
+  transition: margin-left 0.5s;
+  padding: 20px;
+}
+.line {
+  height: 1px;
+}
+@media (max-width: 500px) {
+  .sidenav {
+    padding-top: 30px;
+  }
+}
+/* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
+@media screen and (max-height: 450px) {
+  .sidenav {
+    padding-top: 15px;
+  }
+  /* .sidenav a {
+    font-size: 18px;
+  } */
+}
+.burger {
+  cursor: pointer;
+}
+@media (max-width: 600px) {
+  .line {
+    display: none;
+  }
+}
+/* For open */
+.sidenavOpen {
+  height: 100%; /* 100% Full-height */
+  width: 100%; /* 0 width - change this with JavaScript */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Stay on top */
+  top: 0; /* Stay at the top */
+  left: 0;
+  background-color: #fff; /* Black*/
+  overflow-x: hidden; /* Disable horizontal scroll */
+  padding-top: 60px; /* Place content 60px from the top */
+  transition: 0.5s; /* 0.5 second transition effect to slide in the sidenav */
+}
+/* The navigation menu links */
+/* .sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+} */
+/* When you mouse over the navigation links, change their color */
+.sidenavOpen a:hover {
+  color: #f1f1f1;
+}
+/* Position and style the close button (top right corner) */
+.sidenavOpen .closebtn {
+  font-size: 50px;
+  color: #000;
+}
+/* Style page content - use this if you want to push the page content to the right when you open the side navigation */
+#main {
+  transition: margin-left 0.5s;
+  padding: 20px;
+}
+.line {
+  height: 1px;
+}
+@media (max-width: 500px) {
+  .sidenavOpen {
+    padding-top: 30px;
+  }
+}
+/* On smaller screens, where height is less than 450px, change the style of the sidenavOpen (less padding and a smaller font size) */
+@media screen and (max-height: 450px) {
+  .sidenavOpen {
+    padding-top: 15px;
+  }
+  /* .sidenav a {
+    font-size: 18px;
+  } */
+}
+</style>
